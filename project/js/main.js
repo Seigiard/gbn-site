58d7d5b0b90de3093998db9b576a8edf05522e53
Andrew Borisenko <seigiard@gmail.com>
2018-05-15 21:36:08 +0300

    Remove console.log

--- project/js/main.js
+++ project/js/main.js
@@ -13,13 +13,3 @@ $(function() {
     })
   })
 });
-
-// [...document.querySelectorAll('.info')].forEach(block => {
-//   block.querySelector('.info--toggle').addEventListener('click', (e) => {
-//     if(block.classList.contains(infoBlockClass)) {
-//       block.classList.remove(infoBlockClass);
-//     } else {
-//       block.classList.add(infoBlockClass);
-//     }
-//   });
-// });
