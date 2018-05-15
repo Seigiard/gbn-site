abcf62a3acb899021837a606f7a784f9faa8a26e
Andrew Borisenko <seigiard@gmail.com>
2018-05-16 01:46:40 +0300

    ADD min-width and hire form

--- project/js/main.js
+++ project/js/main.js
@@ -1,14 +1,30 @@
-const infoBlockClass = '.info__content-visible';
+const infoBlockClass = 'info__content-visible';
+
+function getDurationTime($component) {
+  const height = $component.height();
+  const minTime = 300;
+  const maxTime = 900;
+  const msPerHeight = 1;
+  let time = height * msPerHeight
+
+  time = Math.min(time, maxTime);
+  time = Math.max(time, minTime);
+
+  return time;
+}
 
 $(function() {
   $('.service-item').each((id, block) => {
     const $block = $(block);
+    const $content = $('.info--content', $block);
+    const duration = getDurationTime($content);
+
     $('.info--toggle', $block).click(() => {
       const fn = $block.hasClass(infoBlockClass) ? 'slideUp' : 'slideDown';
-      $('.info--content', $block)[fn]({
-        duration: 300,
+      $content[fn]({
+        duration,
+        complete: () => $block.toggleClass(infoBlockClass),
         // easing: "easeInOutSine",
-        complete: () => $block.toggleClass(infoBlockClass)
       })
     })
   })
