7fb070d4f671375a9e0ab905bfd5ca55e56f6483
Andrew Borisenko <seigiard@gmail.com>
2018-05-15 21:35:46 +0300

    ADD easing

--- project/js/main.js
+++ project/js/main.js
@@ -1,11 +1,25 @@
-const infoBlockClass = 'info__content-visible';
-
-[...document.querySelectorAll('.info')].forEach(block => {
-  block.querySelector('.info--toggle').addEventListener('click', (e) => {
-    if(block.classList.contains(infoBlockClass)) {
-      block.classList.remove(infoBlockClass);
-    } else {
-      block.classList.add(infoBlockClass);
-    }
-  });
+const infoBlockClass = '.info__content-visible';
+console.log(123123);
+$(function() {
+  $('.service-item').each((id, block) => {
+    const $block = $(block);
+    $('.info--toggle', $block).click(() => {
+      const fn = $block.hasClass(infoBlockClass) ? 'slideUp' : 'slideDown';
+      $('.info--content', $block)[fn]({
+        duration: 300,
+        // easing: "easeInOutSine",
+        complete: () => $block.toggleClass(infoBlockClass)
+      })
+    })
+  })
 });
+
+// [...document.querySelectorAll('.info')].forEach(block => {
+//   block.querySelector('.info--toggle').addEventListener('click', (e) => {
+//     if(block.classList.contains(infoBlockClass)) {
+//       block.classList.remove(infoBlockClass);
+//     } else {
+//       block.classList.add(infoBlockClass);
+//     }
+//   });
+// });
